faf5651bfa10823c828c2e4a9efd80aa5ea44fbe
Muhamad Iqbal Nurzaman <mhmdnrzmn@gmail.com>
2021-09-24 04:25:31 +0700

    Adding dashboard and Telegram SDK

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Telegram\Bot\Laravel\TelegramServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -174,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+
 
     ],
 
@@ -228,6 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Telegram' => Telegram\Bot\Laravel\Facades\Telegram::class,
 
     ],
 
